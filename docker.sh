2790eba102284dd7984accac77a0c8acc7077ba6
Daniel Payet <danp55@live.fr>
2018-11-17 01:46:02 +0100

    plus besoin de bash, build auto on push vers la prod

--- docker.sh
+++
@@ -1,3 +0,0 @@
-docker build -t danielpayet974/unknowgame .
-docker push danielpayet974/unknowgame
-ssh -i ~/.ssh/id_rsa root@46.101.25.149 'docker stop $(docker ps -q) && docker pull danielpayet974/unknowgame && docker run -p 80:3000 -dit --restart unless-stopped danielpayet974/unknowgame'
